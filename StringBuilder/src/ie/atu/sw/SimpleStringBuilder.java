dfb9b700c0a89026f12fe18adbd7a32f169b3e6d
siog <siog@LAPTOP-545TJ6PE>
2025-05-28 12:49:19 +0100

    Add string value to ssb

--- StringBuilder/src/ie/atu/sw/SimpleStringBuilder.java
+++ StringBuilder/src/ie/atu/sw/SimpleStringBuilder.java
@@ -9,9 +9,7 @@ public class SimpleStringBuilder {
 	
 	public static void main(String[] args) {
 		// Create an instance of the class using the null constructor
-		SimpleStringBuilder ssb = new SimpleStringBuilder();
-		
-		// Take in some values, overload it
+		SimpleStringBuilder ssb = new SimpleStringBuilder("THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG");
 		
 		//Print out the string builder
 		System.out.println(ssb.toString()); // Convert to string representation
