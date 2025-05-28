ec85ea79a0a6c46b41741b8375055e05e1bcd822
siog <siog@LAPTOP-545TJ6PE>
2025-05-28 12:38:20 +0100

    Create a null constructor

--- StringBuilder/src/ie/atu/sw/SimpleStringBuilder.java
+++ StringBuilder/src/ie/atu/sw/SimpleStringBuilder.java
@@ -2,6 +2,11 @@ package ie.atu.sw;
 
 public class SimpleStringBuilder {
 	
+	// Create a null constructor
+	public SimpleStringBuilder() {
+		
+	}
+	
 	public static void main(String[] args) {
 		
 	}
