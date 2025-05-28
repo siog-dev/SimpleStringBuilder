1214d4b7bcccf72f5ccf9ff04a079e84c7292ebc
siog <siog@LAPTOP-545TJ6PE>
2025-05-28 12:45:36 +0100

    Print out the string builder (currently empty)

--- StringBuilder/src/ie/atu/sw/SimpleStringBuilder.java
+++ StringBuilder/src/ie/atu/sw/SimpleStringBuilder.java
@@ -10,6 +10,11 @@ public class SimpleStringBuilder {
 	public static void main(String[] args) {
 		// Create an instance of the class using the null constructor
 		SimpleStringBuilder ssb = new SimpleStringBuilder();
+		
+		// Take in some values, overload it
+		
+		//Print out the string builder
+		System.out.println(ssb.toString()); // Convert to string representation
 	}
 	
 }
