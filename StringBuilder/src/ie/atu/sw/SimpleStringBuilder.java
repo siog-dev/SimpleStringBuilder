fbb1319028742aefdd8454d1929cf8eeb75924e1
siog <siog@LAPTOP-545TJ6PE>
2025-05-28 14:24:16 +0100

    Remove study note comments

--- StringBuilder/src/ie/atu/sw/SimpleStringBuilder.java
+++ StringBuilder/src/ie/atu/sw/SimpleStringBuilder.java
@@ -1,80 +1,58 @@
 package ie.atu.sw;
 
 public class SimpleStringBuilder {
-	// Add default values for clarity (optional)
-	private char[] text = null; // Char = 2 byte/16 bits - use to represent unicode
-	private int index = 0; // Figure out where we are in the index for operations
+	private char[] text = null;
+	private int index = 0;
 	
-	// Create a null constructor
 	public SimpleStringBuilder() {
-	// Need to give it an initial capacity - how long is a piece of string?
-		text = new char[4]; // 4 is a nice power of two - it'll do!
-		
+		text = new char[4];
 	}
-	// Create a constructor that takes a string
+	
 	public SimpleStringBuilder(String s) {
-		// Initialise the constructor public SimpleStringBuilder()
-		this(); // Call the constructor that takes no parameters
+		this();
 		append(s);
 	}
-	// Add methods so that the program will compile
+	
 	public void append(String s) {
-		// Loop over each char in String s and pass each char to append(char c)
 		for (int i = 0; i < s.length(); i++) {
 			append(s.charAt(i));
 		}
 	}
-	// Overload the method - same name, different parameters
-	public void append(char c) { // ALL ROADS LEAD TO ROME
-		// Can append a char
+
+	public void append(char c) {
 		ensureCapacity();
-		text[index] = c; // text[ith position] = c
-		index++; // Increment by index++
+		text[index] = c;
+		index++;
 	}
 	
 	private void ensureCapacity() {
-		// Do a check
-		// 		if my index is greater than or equal to text.length
 		if (index >= text.length) {
-			// Expand
-			// 		Create a temp char array assigned a new char array (current size plus 1)
 			char[] temp = new char[text.length + 1]; 
-			// Copy
-			//		Loop over the old
+
 			for (int i = 0; i < text.length; i++) {
-				// insert into the new one
 				temp[i] = text[i];
 			}
-			// Change/reassign
+
 			text = temp;
 		}
 	}
 	
-	// Loop backwards over the string
 	public String reverse() {
-		// Reverse the array, pass to string constructor because we're returning a string
-		char[] temp = new char[text.length]; // Char array should be the same length as text
+		char[] temp = new char[text.length];
 		
-		// Loop for reversing the array
 		int currentIndex = 0;
-		// Start at zero
 		for (int i = index - 1; i >= 0; i--) {
-			// Insert into currentIndex the character at index i in text
 			temp[currentIndex] = temp[i];
 			currentIndex++;
 		}
 		
-		// Return a string and pass it the char array
 		return new String(temp);
 	}
 	
 	public static void main(String[] args) {
-		// Create an instance of the class using the null constructor
 		SimpleStringBuilder ssb = new SimpleStringBuilder("THEQUICKBROWNFOXJUMPS");
-		ssb.append("OVERTHELAZYDOG"); // Append end of sentence efficiently
-		//Print out the string builder
-		System.out.println(ssb.toString()); // Convert to string representation
+		ssb.append("OVERTHELAZYDOG");
+		System.out.println(ssb.toString());
 		System.out.println(ssb.reverse());
 	}
-	
 }
