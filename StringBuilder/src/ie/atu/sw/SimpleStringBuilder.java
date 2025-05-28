0acea0dd67a982b44d7beb4dd040b8089c285313
siog <siog@LAPTOP-545TJ6PE>
2025-05-28 12:55:48 +0100

    Create a constructor that takes a string

--- StringBuilder/src/ie/atu/sw/SimpleStringBuilder.java
+++ StringBuilder/src/ie/atu/sw/SimpleStringBuilder.java
@@ -6,6 +6,10 @@ public class SimpleStringBuilder {
 	public SimpleStringBuilder() {
 		
 	}
+	// Create a constructor that takes a string
+	public SimpleStringBuilder(String s) {
+		
+	}
 	
 	public static void main(String[] args) {
 		// Create an instance of the class using the null constructor
